c80f768511cc9cde19d26235b63e07aa64e4f0f2
Robin Métral <robin@metral.ch>
2019-01-21 22:16:43 +0100

    fetch() gojson countries geometry

--- scripts.js
+++ scripts.js
@@ -1,8 +1,22 @@
-const been = L.map("mapid").setView([44, 12], 2);
+// set default map view
+const map = L.map("mapid").setView([44, 12], 2);
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>',
+// mapbox public access token
+const mapboxToken = "pk.eyJ1Ijoicm9iaW5tZXRyYWwiLCJhIjoiY2pkMTI0bWVnMmV6dzM0bnNhZHBvMDBqeiJ9.Z0gZrvkth24hNkLkvRxg-g";
+
+// set tile layer
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=" + mapboxToken, {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, Imagery &copy; <a href=\"https://www.mapbox.com/\">Mapbox</a>",
   maxZoom: 18,
   id: "mapbox.light",
-  accessToken: "pk.eyJ1Ijoicm9iaW5tZXRyYWwiLCJhIjoiY2pkMTI0bWVnMmV6dzM0bnNhZHBvMDBqeiJ9.Z0gZrvkth24hNkLkvRxg-g"
-}).addTo(been);
+}).addTo(map);
+
+// fetch geojson countries geometry
+fetch("./countries.geojson")
+  .then(response => {
+    return response.json();
+  }).then(data => {
+    L.geoJson(data).addTo(map);
+  }).catch(err => {
+    console.log("There was an error.");
+  });
